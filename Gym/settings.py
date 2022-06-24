441fb350d3ac6a7491a1677e5384171b6c5ff6b5
maxym1995 <91147427+maxym1995@users.noreply.github.com>
2022-06-24 14:57:16 +0200

    Update settings.py

--- Gym/settings.py
+++ Gym/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'HOST': '127.0.0.1',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gym_v2',
-        'USER': 'postgres',
+        'NAME': '',
+        'USER': '',
         'PASSWORD': 'coderslab',
     }
 }
